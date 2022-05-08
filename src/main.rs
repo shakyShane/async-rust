441eab13affaf218c08a2a27af9febf347d38dfd
Shane Osbourne <shane.osbourne8@gmail.com>
2022-05-08 21:05:33 +0100

    hang

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use futures::channel::mpsc;
+use futures::executor::block_on;
+use futures::StreamExt;
+
+fn main() {
+    block_on(async {
+        let (_sender, receiver) = mpsc::channel::<()>(1);
+        receiver.collect::<Vec<_>>().await
+    });
+}
